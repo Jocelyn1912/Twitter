1d802bb31f16711219ad9bd9a42856269580600d
Jocelyn1912 <toty_2410@hotmail.com>
2017-11-11 20:16:01 -0300

    Cambio de color en el contador, bloqueo de boton y mejoras

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,8 @@ function add() {
 
 	//Si el contenedor está vacío deshabilitar el botón de tweet
 	if(tweets.length == 0 || tweets == null) {
-		return document.getElementById('tweet')
+		return document.getElementById('tweet');
+		//document.getElementById('boton').disabled = true;		
 	}
 
 	//Agregando elementos al DOM
@@ -26,9 +27,28 @@ function add() {
 //Contador de caracteres
 var tweetTextarea = document.getElementById('tweet');
 var counterMax = document.getElementById('tweetCounter');
-//Función que contará cada caracter en la longitud de la oración y le descontará 140 para ir obteniendo el valor del contador. 
+
+/*
+Función que contará cada caracter en la longitud de la oración
+y le descontará 140 para ir obteniendo el valor del contador. 
+*/
 tweetTextarea.onkeydown = function () {	
-	counterMax.innerHTML = 140 - this.value.length;		
+	var currentCounter = counterMax.innerHTML = 140 - this.value.length;
+	//Si el contador es menor a 0 desactivar el botón de twittear
+	if (currentCounter < 0){
+		document.getElementById('boton').disabled = true;
+
+	//Si el contador es menor a 21 y mayor a 10 debe ponerse rojo
+	} if (currentCounter < 21 && currentCounter > 10) {
+		counterMax.classList.add('red');
+	//Si el contador es menor a 11 y mayor a 0 debe ponerse amarillo
+	} if (currentCounter < 11 && currentCounter > 0 ) {
+		counterMax.classList.add('yellow');
+	//Si el contador es menor a 0 se deben remover los colores
+	} if (currentCounter < 0 ){
+		counterMax.classList.remove('yellow');
+		counterMax.classList.remove('red');
+	}
 };
 
 /*
@@ -41,10 +61,14 @@ function checkLength(textField) {
  	}
 }
 */
+
+/*
+Función que hace crecer al textarea de acuerdo 
+al contenido y así no se crea el scroll
+*/
 var textarea = document.getElementById('tweet');
-var limit = 200;
 
 textarea.oninput = function() {
   textarea.style.height = '';
-  textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
+  textarea.style.height = Math.min(textarea.scrollHeight, 200) + "px";
 };
